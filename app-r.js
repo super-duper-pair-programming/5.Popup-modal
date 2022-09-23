cd3036847d21fd44b649538319e78cf70cbacca7
chaerin-dev <chaerin.dev@gmail.com>
2022-09-23 12:35:21 +0900

    refactor: refactoring

--- app-r.js
+++ app-r.js
@@ -1,20 +1,33 @@
+const $togglePopup = document.querySelector('.toggle-popup');
 const $popupModal = document.querySelector('.popup-modal');
-
-document.querySelector('.toggle-popup').addEventListener('click', () => $popupModal.classList.add('open'));
-
+const $modalForm = document.querySelector('.modal-form');
 const $modalInput = document.querySelector('.modal-input');
+const $popupMessage = document.querySelector('.popup-message');
+
+const openModal = () => $popupModal.classList.add('open');
 
 const closeModal = () => {
-  $modalInput.value = '';
+  $modalForm.reset();
   $popupModal.classList.remove('open');
 };
+
+const renderPopupMsg = input => {
+  $popupMessage.textContent = `from popup : ${input}`;
+};
+
+$togglePopup.addEventListener('click', openModal);
 
 $popupModal.addEventListener('click', e => {
   if (e.target.matches(':not(.popup-modal *), .cancel-btn, .bx-x')) closeModal();
 });
 
-document.querySelector('.modal-form').addEventListener('submit', e => {
+$modalForm.addEventListener('submit', e => {
   e.preventDefault();
-  document.querySelector('.popup-message').textContent = `from popup : ${$modalInput.value}`;
+  renderPopupMsg($modalInput.value);
   closeModal();
 });
+
+// [변경사항]
+// - opemModal, closeModal, renderPopupMsg 함수를 생성하여 관련 코드를 event listener에서 분리함
+// - $modalInput.value = '' 대신 $modalForm.reset()을 사용하여 form을 초기화한다는 것을 명시적으로 표현
+// - event listener 내에서 CSS selector를 활용하여 코드 간소화
